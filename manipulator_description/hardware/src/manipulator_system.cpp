cf62bc98c72f54b89097e66065b94a53b2e7142f
Tadeusz Chmielik <t.j.chmielik@gmail.com>
2025-07-29 23:45:27 +0200

    cleaned code

--- manipulator_description/hardware/src/manipulator_system.cpp
+++ manipulator_description/hardware/src/manipulator_system.cpp
@@ -1,111 +1,3 @@
-// #include "manipulator_hw/manipulator_system.hpp"
-// #include "manipulator_hw/serial_comm.hpp"  // Klasa do komunikacji UART
-// #include "pluginlib/class_list_macros.hpp"
-// #include <rclcpp/rclcpp.hpp>
-
-// namespace manipulator_hw
-// {
-
-// using hardware_interface::CallbackReturn;
-// using hardware_interface::return_type;
-
-// CallbackReturn ManipulatorSystem::on_init(const hardware_interface::HardwareInfo &info)
-// {
-//   RCLCPP_INFO(rclcpp::get_logger("ManipulatorSystem"), "on_init called");
-
-//   if (SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
-//     return CallbackReturn::ERROR;
-
-//   for (size_t i = 0; i < NUM_JOINTS; ++i) {
-//     joint_names_[i] = info.joints[i].name;
-//     pos_[i] = 0.0;
-//     vel_[i] = 0.0;
-//     cmd_[i] = 0.0;
-//   }
-
-//   return CallbackReturn::SUCCESS;
-// }
-
-// CallbackReturn ManipulatorSystem::on_configure(const rclcpp_lifecycle::State &)
-// {
-//   if (!serial_comm_.connect("/dev/ttyAMA0", 115200)) {
-//     RCLCPP_ERROR(rclcpp::get_logger("ManipulatorSystem"), "Failed to open serial port");
-//     return CallbackReturn::ERROR;
-//   }
-
-//   RCLCPP_INFO(rclcpp::get_logger("ManipulatorSystem"), "Serial connected successfully");
-//   return CallbackReturn::SUCCESS;
-// }
-
-// CallbackReturn ManipulatorSystem::on_activate(const rclcpp_lifecycle::State &)
-// {
-//   RCLCPP_INFO(rclcpp::get_logger("ManipulatorSystem"), "System activated");
-//   return CallbackReturn::SUCCESS;
-// }
-
-// CallbackReturn ManipulatorSystem::on_deactivate(const rclcpp_lifecycle::State &)
-// {
-//   RCLCPP_INFO(rclcpp::get_logger("ManipulatorSystem"), "System deactivated");
-//   return CallbackReturn::SUCCESS;
-// }
-
-// CallbackReturn ManipulatorSystem::on_cleanup(const rclcpp_lifecycle::State &)
-// {
-//   serial_comm_.disconnect();
-//   RCLCPP_INFO(rclcpp::get_logger("ManipulatorSystem"), "Serial disconnected");
-//   return CallbackReturn::SUCCESS;
-// }
-
-// std::vector<hardware_interface::StateInterface> ManipulatorSystem::export_state_interfaces()
-// {
-//   std::vector<hardware_interface::StateInterface> state;
-//   for (size_t i = 0; i < NUM_JOINTS; ++i) {
-//     state.emplace_back(joint_names_[i], hardware_interface::HW_IF_POSITION, &pos_[i]);
-//     state.emplace_back(joint_names_[i], hardware_interface::HW_IF_VELOCITY, &vel_[i]);
-//   }
-//   return state;
-// }
-
-// std::vector<hardware_interface::CommandInterface> ManipulatorSystem::export_command_interfaces()
-// {
-//   std::vector<hardware_interface::CommandInterface> cmd;
-//   for (size_t i = 0; i < NUM_JOINTS; ++i) {
-//     cmd.emplace_back(joint_names_[i], hardware_interface::HW_IF_POSITION, &cmd_[i]);
-//   }
-//   return cmd;
-// }
-
-// return_type ManipulatorSystem::read(const rclcpp::Time &, const rclcpp::Duration &)
-// {
-//   std::array<double, NUM_JOINTS> new_positions;
-//   if (serial_comm_.read_positions(new_positions)) {
-//     pos_ = new_positions;
-//     RCLCPP_DEBUG(rclcpp::get_logger("ManipulatorSystem"), "Read positions from serial");
-//   }
-//   return return_type::OK;
-// }
-
-// return_type ManipulatorSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
-// {
-//   serial_comm_.send_commands(cmd_);
-//   RCLCPP_DEBUG(rclcpp::get_logger("ManipulatorSystem"), "Sent commands to serial");
-//   return return_type::OK;
-// }
-
-// }  // namespace manipulator_hw
-
-// PLUGINLIB_EXPORT_CLASS(manipulator_hw::ManipulatorSystem, hardware_interface::SystemInterface)
-
-
-
-
-
-
-
-
-
-
-
 #include "manipulator_hw/manipulator_system.hpp"
 #include <termios.h>
 #include <fcntl.h>
@@ -148,6 +40,15 @@ CallbackReturn ManipulatorSystem::on_configure(const rclcpp_lifecycle::State &)
   return CallbackReturn::SUCCESS;
 }
 
+CallbackReturn ManipulatorSystem::on_cleanup(const rclcpp_lifecycle::State &)
+{
+  if (fd_ >= 0) {
+    close(fd_);
+    fd_ = -1;
+    RCLCPP_INFO(rclcpp::get_logger("ManipulatorSystem"), "Serial port closed");
+  }
+  return CallbackReturn::SUCCESS;
+}
 
 
 
@@ -173,39 +74,6 @@ std::vector<hardware_interface::CommandInterface> ManipulatorSystem::export_comm
   return cmd;
 }
 
-// return_type ManipulatorSystem::read(const rclcpp::Time &, const rclcpp::Duration &)
-// {
-//   char buf[512] = {};
-//   int n = ::read(fd_, buf, sizeof(buf));
-//   if (n > 0) {
-//     buffer_ += std::string(buf, n);
-//     auto pos_nl = buffer_.find("\n");
-//     if (pos_nl != std::string::npos) {
-//       std::string line = buffer_.substr(0, pos_nl);
-//       buffer_.erase(0, pos_nl+1);
-//       std::replace(line.begin(), line.end(), ',', ' ');
-//       std::istringstream ss(line);
-//       double j1,j2,j3; int g;
-//       if (ss >> j1 >> j2 >> j3 >> g) {
-//         RCLCPP_INFO(rclcpp::get_logger("ManipulatorSystem"), "Received: j1=%.3f j2=%.3f j3=%.3f g=%d", j1, j2, j3, g);
-
-//         pos_[0]=j1; pos_[1]=j2; pos_[2]=j3;
-//         // pos_[0]=cmd_[0]; pos_[1]=cmd_[1]; pos_[2]=cmd_[3];
-        
-//         pos_[3]=cmd_[3];
-//         pos_[4]=pos_[5]=cmd_[4];
-//       }
-//     }
-//   }
-//   else {
-//     RCLCPP_WARN(rclcpp::get_logger("ManipulatorSystem"), "No data from serial - injecting dummy values");
-//     for (size_t i = 0; i < NUM_JOINTS; ++i) {
-//       pos_[i] = cmd_[i]; // lub np. pos_[i] = 0.0;
-//       vel_[i] = 0.0;
-//     }
-//   }
-//   return return_type::OK;
-// }
 
 return_type ManipulatorSystem::read(const rclcpp::Time &, const rclcpp::Duration &)
 {
@@ -220,7 +88,7 @@ return_type ManipulatorSystem::read(const rclcpp::Time &, const rclcpp::Duration
 
       if (start != std::string::npos && end != std::string::npos && end > start) {
         std::string frame = buffer_.substr(start + 1, end - start - 1);
-        buffer_.erase(0, end + 1);  // usuń przetworzoną ramkę
+        buffer_.erase(0, end + 1); 
 
         std::replace(frame.begin(), frame.end(), ',', ' ');
         std::istringstream ss(frame);
@@ -237,7 +105,6 @@ return_type ManipulatorSystem::read(const rclcpp::Time &, const rclcpp::Duration
         pos_[4]=pos_[5]=cmd_[4];
         }
       } else {
-        // brak kompletnej ramki, poczekaj na więcej danych
         break;
       }
     }
@@ -256,7 +123,7 @@ return_type ManipulatorSystem::read(const rclcpp::Time &, const rclcpp::Duration
 return_type ManipulatorSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
 {
   char out[128];
-  int gripper_state = 0;  // dostosuj w razie potrzeby
+  int gripper_state = 0; 
 
   snprintf(out, sizeof(out), "<%.3f,%.3f,%.3f,%.3f,%d>", 
            cmd_[0], cmd_[1], cmd_[2], cmd_[3], gripper_state);
